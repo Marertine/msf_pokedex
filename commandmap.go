6b5c54dd24ff654c9d3ab7350ac512b213446f85
Marertine <rhodrirhamantus@gmail.com>
2026-01-01 14:29:23 +1100

    Ch2-5: Cheated

--- commandmap.go
+++ commandmap.go
@@ -49,7 +49,11 @@ func getCommands() map[string]cliCommand {
 			description: "Displays a help message",
 			callback:    commandHelp,
 		},
-		"map": {
+		"inspect": {
+			name:        "inspect",
+			description: "See details about a Pokemon",
+			callback:    commandInspect,
+		}, "map": {
 			name:        "map",
 			description: "Display 20 locations at a time",
 			callback:    commandMap,
@@ -202,5 +206,30 @@ func commandCatch(cfg *config, args ...string) error {
 		fmt.Printf("%s escaped!\n", pokemon.Name)
 	}
 
+	return nil
+}
+
+func commandInspect(cfg *config, args ...string) error {
+	if len(args) != 1 {
+		return errors.New("you must provide a pokemon name")
+	}
+
+	name := args[0]
+	pokemon, ok := cfg.caughtPokemon[name]
+	if !ok {
+		return errors.New("you have not caught that pokemon")
+	}
+
+	fmt.Println("Name:", pokemon.Name)
+	fmt.Println("Height:", pokemon.Height)
+	fmt.Println("Weight:", pokemon.Weight)
+	fmt.Println("Stats:")
+	for _, stat := range pokemon.Stats {
+		fmt.Printf("  -%s: %v\n", stat.Stat.Name, stat.BaseStat)
+	}
+	fmt.Println("Types:")
+	for _, typeInfo := range pokemon.Types {
+		fmt.Println("  -", typeInfo.Type.Name)
+	}
 	return nil
 }
